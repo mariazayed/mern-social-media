3c1976d6be398548bf260f333a5f7c0f5c92287f
Maria Zayed <mariawzayed@gmail.com>
2021-10-14 16:18:12 +0300

    Minor fix

--- backend/graphql/resolvers/posts.js
+++ backend/graphql/resolvers/posts.js
@@ -10,8 +10,7 @@ module.exports = {
 	Query: {
 		async getPosts() {
 			try {
-				const posts = await Post.find().sort({createdAt: -1});
-				return posts;
+				return await Post.find().sort({createdAt: -1});
 			}
 			catch (err) {
 				throw new Error(err);
