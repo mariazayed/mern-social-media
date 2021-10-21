e1a39f054f9b472f9b0452fe1d76b754c9bfa585
Maria Zayed <mariawzayed@gmail.com>
2021-10-21 13:58:58 +0300

    Minor fix

--- frontend/src/util/graphql.js
+++ frontend/src/util/graphql.js
@@ -1,23 +1,23 @@
-import gql from 'graphql-tag';
+import gql from "graphql-tag";
 
 export const FETCH_POSTS_QUERY = gql`
-  {
-    getPosts {
-      id
-      body
-      createdAt
-      username
-      likeCount
-      likes {
-        username
-      }
-      commentCount
-      comments {
-        id
-        username
-        createdAt
-        body
-      }
+    {
+        getPosts {
+            id
+            body
+            createdAt
+            username
+            likeCount
+            likes {
+                username
+            }
+            commentCount
+            comments {
+                id
+                username
+                createdAt
+                body
+            }
+        }
     }
-  }
 `;
